728ce9be40495a02023edb59b3dca041bbd31f31
kuojsean <kuo.j.sean@gmail.com>
2018-04-16 08:16:54 -0600

    Started setting up the frame of the app.

--- src/create/controller/MaintenanceController.java
+++ src/create/controller/MaintenanceController.java
@@ -18,7 +18,50 @@ public class MaintenanceController
 	
 	private void setupBusinessList()
 	{
+		businessList.add(new DraperPlumbing());
+		businessList.add(new HillcrestHandyman());
+		businessList.add(new MidvaleMowing());
+		businessList.add(new SandyLawncare());
+		businessList.add(new WasatchRepair());
+	}
+	
+	public List<Business> getBusinessList()
+	{
+		return businessList;
+	}
+	
+	public boolean isValidInteger(String input)
+	{
+		boolean valid = false;
 		
+		try
+		{
+			Integer.parseInt(input);
+			valid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			System.out.println("Only integer values are valid.");
+		}
+		
+		return valid;
+	}
+	
+	public boolean isValidDouble(String input)
+	{
+		boolean valid = false;
+		
+		try
+		{
+			Double.parseDouble(input);
+			valid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			System.out.println("Only double values are valid.");
+		}
+		
+		return valid;
 	}
 	
 	public void start()
--- src/create/view/MaintenancePanel.java
+++ src/create/view/MaintenancePanel.java
@@ -1,17 +1,52 @@
 package create.view;
 
 import create.controller.*;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import java.awt.Color;
-import javax.swing.SpringLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class MaintenancePanel extends JPanel
 {
 
 	private MaintenanceController appController;
-	private JButton Button;
+	private SpringLayout baseLayout;
+	
+	private JButton plumbingButton;
+	private JButton electricianButton;
+	private JButton lawncareButton;
+	
+	private JButton choiceOne;
+	private JButton choiceTwo;
+	
+	private JTextArea description;
+	private JPanel firstType;
+	private JPanel secondType;
+	
+	public MaintenancePanel (MaintenanceController appController) 
+	{
+		super();
+		this.appController = appController;
+		
+		baseLayout = new SpringLayout();
+		
+		plumbingButton = new JButton("Plumbing");
+		electricianButton = new JButton("Electrician");
+		lawncareButton = new JButton("Lawncare");
+		
+		choiceOne = new JButton("Option 1");
+		choiceTwo = new JButton("Option 2");
+		
+		description = new JTextArea();
+		firstType = new JPanel();
+		secondType = new JPanel();
+	}
+	
+	private void setupPanel()
+	{
+		this.setBackground(Color.CYAN);
+		this.setLayout(baseLayout);
+		
+		this.add(plumbingButton);
+	}
 	
 }
