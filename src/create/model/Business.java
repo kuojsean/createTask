b0866d94aae91ca95ec8ccea4bc1ab846dff8e22
kuojsean <kuo.j.sean@gmail.com>
2018-04-16 07:38:06 -0600

    Added updates from code at home.

--- src/create/model/Business.java
+++ src/create/model/Business.java
@@ -5,11 +5,10 @@ import java.util.*;
 public abstract class Business
 {
 	private int businessNumber;
-	private int phoneNumber;
+	private long phoneNumber;
 	private int priceRangeLow;
 	private int priceRangeHigh;
 	private String businessName;
-	private String maintenanceType;
 	private String address;
 	private String hoursOpen;
 	
@@ -17,6 +16,37 @@ public abstract class Business
 	{
 		this.businessNumber = businessNumber;
 		this.businessName = businessName;
+	}
+	
+	public String[] getBusinessTypes()
+	{
+		String [] type = null;
+		ArrayList<String> parentType = new ArrayList<String>();
+		Class<?> currentBusiness = this.getClass();
+		
+		while(currentBusiness.getSuperclass() != null)
+		{
+			Class<?> [] businessTypes = currentBusiness.getInterfaces();
+			type = new String[businessTypes.length];
+			
+			for (int index = 0; index < type.length; index++)
+			{
+				String currentInterface = businessTypes[index].getCanonicalName();
+				if(!parentType.contains(currentInterface))
+				{
+					parentType.add(currentInterface);
+				}
+			}
+			currentBusiness = currentBusiness.getSuperclass();
+		}
+		
+		type = new String [parentType.size()];
+		
+		for(int index = 0; index < parentType.size(); index ++)
+		{
+			type[index] = parentType.get(index);
+		}
+		return type;
 	}
 	
 	public String toString()
@@ -30,7 +60,7 @@ public abstract class Business
 		return businessNumber;
 	}
 	
-	public int getPhoneNumber()
+	public long getPhoneNumber()
 	{
 		return phoneNumber;
 	}
@@ -58,5 +88,35 @@ public abstract class Business
 	private String getHours()
 	{
 		return hoursOpen;
+	}
+	
+	public void setPhoneNumber(long phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public void setLowPrice(int priceRangeLow)
+	{
+		this.priceRangeLow = priceRangeLow;
+	}
+	
+	public void setHighPrice(int priceRangeHigh)
+	{
+		this.priceRangeHigh = priceRangeHigh;
+	}
+	
+	public void setBusinessName(String businessName)
+	{
+		this.businessName = businessName;
+	}
+	
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+	
+	public void setHours(String hoursOpen)
+	{
+		this.hoursOpen = hoursOpen;
 	}
 }
